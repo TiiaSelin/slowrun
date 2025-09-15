b2be154976e21c70a670ec9f67da0a98559ddfb0
TiiaSelin <tiiaselin92@gmail.com>
2025-09-15 18:06:03 +0300

    luotu kontaktilomakeelle urls.py ja määritelty tarvittavat reitit

---
+++ kontaktilomake/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    #path('yhteystiedot/', views.yhteystiedot_view, name='yhteystiedot'),  
+]
