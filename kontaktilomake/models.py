462bd3583e7dc603061fc3a841a08605ba8663dd
TiiaSelin <tiiaselin92@gmail.com>
2025-09-15 16:57:06 +0300

    luotu malli kontaktilomakkeelle

--- kontaktilomake/models.py
+++ kontaktilomake/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Viesti(models.Model):
+    nimi = models.CharField(max_length=100)
+    sahkoposti = models.EmailField()
+    aihe = models.CharField(max_length=200)
+    viesti = models.TextField()
+    luotu = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nimi
+
