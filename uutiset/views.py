e7a54d677235ac67838b33f89c254ddb244ceca6
ExcavatorZ <excavatorz@hotmail.fi>
2025-09-16 16:03:39 +0300

    Luotu dynaaminen uutissivujen luonti.

--- uutiset/views.py
+++ uutiset/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Uutinen
 
 def uutiset_view(request):
     uutiset = Uutinen.objects.all().order_by("date")
     return render(request, "uutiset.html", {"uutiset": uutiset})
     
+def uutinen_detail(request, uutinen_id):
+    uutinen = get_object_or_404(Uutinen, id=uutinen_id)
+    return render(request, "uutinen_detail.html", {"uutinen": uutinen})
