ee22a94fe7d36b09e9cc1ab98d13145e41e3e8e3
TiiaSelin <tiiaselin92@gmail.com>
2025-09-17 15:21:36 +0300

    lisätty toiminnallisuus yhteystiedot sivun kontaktilomakkeelle

--- kontaktilomake/views.py
+++ kontaktilomake/views.py
@@ -23,3 +23,26 @@ def index_view(request):
         
     
     return render(request, 'index.html', {'viesti_lahetetty': viesti_lahetetty})
+
+
+def yhteystiedot_view(request):
+    viesti_lahetetty = False
+
+    if request.method == "POST":
+        nimi = request.POST.get('nimi')
+        sahkoposti = request.POST.get('sahkoposti')
+        paikkakunta = request.POST.get('paikkakunta')
+        viesti = request.POST.get('viesti')
+
+        Viesti.objects.create(
+            nimi=nimi,
+            sahkoposti=sahkoposti,
+            paikkakunta=paikkakunta,
+            viesti=viesti
+        )
+        viesti_lahetetty = True
+        
+    return render(request, "yhteystiedot.html", {"viesti_lahetetty": viesti_lahetetty})
+
+
+
