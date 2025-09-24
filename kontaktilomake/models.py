52493196837217f4f1162db6f852e9265a9f9a52
ExcavatorZ <excavatorz@hotmail.fi>
2025-09-24 18:57:54 +0300

    Luotu malli etusivukuvalle ja sisällytetty se etusivulle.

--- kontaktilomake/models.py
+++ kontaktilomake/models.py
@@ -10,3 +10,9 @@ class Viesti(models.Model):
     def __str__(self):
         return self.nimi
 
+class Etusivukuva(models.Model):
+    picture = models.ImageField(upload_to="")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Kuva lisätty: {self.uploaded_at:%y-%m-%d}"
