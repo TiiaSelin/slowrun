101bea98ece8a36c0875d2c2a6540cff3da6c083
ExcavatorZ <excavatorz@hotmail.fi>
2025-09-24 16:39:52 +0300

    Lisätty signaali, joka poistaa kuvatiedoston uutisen poistuessa ja kirjoitettu testi.

--- uutiset/views.py
+++ uutiset/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from .models import Uutinen
 
 def uutiset_view(request):
@@ -8,3 +10,9 @@ def uutiset_view(request):
 def uutinen_detail(request, uutinen_id):
     uutinen = get_object_or_404(Uutinen, id=uutinen_id)
     return render(request, "uutinen_detail.html", {"uutinen": uutinen})
+
+@receiver(post_delete, sender=Uutinen)
+def delete_picture_on_uutinen_deletion(sender, instance, **kwargs):
+    if instance.picture:
+        print(instance.picture)
+        instance.picture.delete(save=False)
