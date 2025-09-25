e6a908f4788a16e92ce5edfc6a054153e730f99f
TiiaSelin <tiiaselin92@gmail.com>
2025-09-25 17:48:05 +0300

    kontaktilomakkeen views.py koodia paranneltu

--- kontaktilomake/views.py
+++ kontaktilomake/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.templatetags.static import static
 from .models import Viesti, Etusivukuva
 
-def index_view(request):
-    front_image = Etusivukuva.objects.last()
-    viesti_lahetetty = False
-    
+def laheta_viesti(request):
     if request.method == "POST":
         nimi = request.POST.get('nimi')
         sahkoposti = request.POST.get('sahkoposti')
@@ -18,35 +15,31 @@ def index_view(request):
             paikkakunta=paikkakunta,
             viesti=viesti
         )
-        viesti_lahetetty = True
+        return True
+    return False
 
+
+
+def index_view(request):
+    front_image = Etusivukuva.objects.last()
+    viesti_lahetetty = laheta_viesti(request)
     if front_image:
         img_url = front_image.picture.url
     else:
         img_url = static("images/pexels-drerun-22541351.jpg")
         
-    
     return render(request, 'index.html', {'viesti_lahetetty': viesti_lahetetty, "img_url": img_url})
 
 
 def yhteystiedot_view(request):
-    viesti_lahetetty = False
-
-    if request.method == "POST":
-        nimi = request.POST.get('nimi')
-        sahkoposti = request.POST.get('sahkoposti')
-        paikkakunta = request.POST.get('paikkakunta')
-        viesti = request.POST.get('viesti')
-
-        Viesti.objects.create(
-            nimi=nimi,
-            sahkoposti=sahkoposti,
-            paikkakunta=paikkakunta,
-            viesti=viesti
-        )
-        viesti_lahetetty = True
-        
+    viesti_lahetetty = laheta_viesti(request)
+    
     return render(request, "yhteystiedot.html", {"viesti_lahetetty": viesti_lahetetty})
+
+
+
+        
+
 
 
 
